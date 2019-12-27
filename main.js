e71ac7dd17c7ed61ea9d74adb0633f6ce2bb189e
Sameliuk Oleg <715367@gmail.com>
2019-12-27 23:43:14 +0200

    result

--- main.js
+++ main.js
@@ -1,55 +1,60 @@
-function plus(a,b) {
+function myPlus(a,b) {
   return a+b;
 }
 
-plus(5,7);
+myPlus(5,7);
 
-function subtraction(a,b) {
+function mySubtraction(a,b) {
   return a-b;
 }
 
-subtraction(9,6);
+mySubtraction(9,6);
 
-function multiplication(a,b) {
+function myMultiplication(a,b) {
   return a*b;
 }
-multiplication(5,6);
+myMultiplication(5,6);
 
 
-function division(a,b) {
+function myDivision(a,b) {
   return a/b;
 }
 
-division(9,5);
+myDivision(9,5);
 
-function power(a,b) {
+function myPower(a,b) {
   return a**b;
 }
 
-power(2,3);
+myPower(2,3);
 
-function root(a,b) {
+function myRoot(a,b) {
   return a**(1/b);
 }
 
-root(81,3);
+myRoot(81,3);
 
 
 
-function cos (a) {
+function myCos (a) {
   return Math.cos(a);
 }
 
-cos(5);
+myCos(5);
 
-function sin (a) {
+function mySin (a) {
   return Math.sin(a);
 }
-sin(5);
+mySin(5);
 
 
-function fac(a) {
-  return a ? a * fac(a - 1) : 1;
+function myFac(a) {
+  return a ? a * myFac(a - 1) : 1;
+  let b = a;
+  for (let i = b-1; i > 0; i--) {
+    b = b * i
+  }
+  return b;
 }
 
-fac (6);
+myFac (6);
